3a7da76d3768f2be8520c5ff4d4e3387596d1c26
xDouglas90 <xdouglas90@gmail.com>
2022-06-05 17:51:38 -0300

    feat: finish CategoryPosts page

--- src/pages/CategoryPosts/index.jsx
+++ src/pages/CategoryPosts/index.jsx
@@ -1,10 +1,66 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useLayoutEffect, useEffect, useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+
+import { PostItem } from '../../components';
+
+import api from '../../services/api';
 
 export function CategoryPosts() {
+  const [postsList, setPostsList] = useState([]);
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: route.params?.title,
+    });
+  }, [navigation, route]);
+
+  useEffect(() => {
+    async function loadPosts() {
+      await getListPosts();
+    }
+
+    loadPosts();
+  }, []);
+
+  const getListPosts = async () => {
+    const response = await api.get(
+      `api/categories/${route.params?.id}?fields=name&populate=posts,posts.cover`
+    );
+    setPostsList(response.data?.data?.attributes?.posts?.data);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Página posts de uma categoria</Text>
+      {postsList.length === 0 ? (
+        <>
+          <Text style={styles.resultText}>
+            Essa categoria ainda não possui nenhum post.
+          </Text>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.goBack()}
+          >
+            <Text style={styles.buttonText}>Encontrar posts</Text>
+          </TouchableOpacity>
+        </>
+      ) : (
+        <FlatList
+          style={styles.posts}
+          data={postsList}
+          showsVerticalScrollIndicator={false}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => <PostItem data={item} />}
+        />
+      )}
     </View>
   );
 }
@@ -12,18 +68,26 @@ export function CategoryPosts() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 18,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  title: {
-    color: '#262626',
-    fontSize: 30,
+  resultText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 20,
     textAlign: 'center',
   },
-  subtitle: {
-    color: '#262626',
-    fontSize: 20,
-    textAlign: 'center',
+  button: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: '#162133',
+    borderRadius: 5,
+    width: '50%',
+    alignSelf: 'center',
   },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    textAlign: 'center',
+  }
 });
